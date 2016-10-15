35029aa7074be377c24211a58e4453a025b912f9
Verena <verry@zetcad.de>
2016-10-15 03:00:15 +0200

    added apiservice and dataservice

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,30 +1,33 @@
-import { NgModule } from '@angular/core';
-import { IonicApp, IonicModule } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { PracticePage } from '../pages/practice/practice';
-import { StatisticPage } from '../pages/statistic/statistic';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import {NgModule} from '@angular/core';
+import {IonicApp, IonicModule} from 'ionic-angular';
+import {MyApp} from './app.component';
+import {PracticePage} from '../pages/practice/practice';
+import {StatisticPage} from '../pages/statistic/statistic';
+import {HomePage} from '../pages/home/home';
+import {TabsPage} from '../pages/tabs/tabs';
+import {DataService} from "../providers/dataservice";
+import {APIService} from "../providers/apiservice";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    PracticePage,
-    StatisticPage,
-    HomePage,
-    TabsPage
-  ],
-  imports: [
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    PracticePage,
-    StatisticPage,
-    HomePage,
-    TabsPage
-  ],
-  providers: []
+    declarations: [
+        MyApp,
+        PracticePage,
+        StatisticPage,
+        HomePage,
+        TabsPage
+    ],
+    imports: [
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        PracticePage,
+        StatisticPage,
+        HomePage,
+        TabsPage
+    ],
+    providers: [DataService, APIService]
 })
-export class AppModule {}
+export class AppModule {
+}
